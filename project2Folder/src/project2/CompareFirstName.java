47ef970b04670ea8872bceaf3971cb692088d02c
dem1995 <belmont95@live.com>
2015-02-18 19:59:26 -0600

    Renamed file to fit submission requirements
    Project renamed to "project2"

---
+++ project2Folder/src/project2/CompareFirstName.java
@@ -0,0 +1,23 @@
+package project2;
+
+import java.util.Comparator;
+
+/**
+ * Project #2
+ * CS 2334, Section 010
+ * Feb 18, 2015
+ * <p>
+ * A comparator object that takes in two <code>Person</code>s and compares their <code>firstName</code>s
+ * </p>
+ * @version 1.0
+ */
+public class CompareFirstName implements Comparator<Person>{
+
+
+
+	public int compare(Person person1, Person person2) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
