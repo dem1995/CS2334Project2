681325e1466014843c90092d2e9abf537e444369
dem1995 <belmont95@live.com>
2015-04-12 13:29:05 -0500

    Updated Driver, updated and added SelectionView

--- project2Folder/src/MVCStuff/Driver.java
+++ project2Folder/src/MVCStuff/Driver.java
@@ -2,12 +2,36 @@ package MVCStuff;
 
 import java.util.ArrayList;
 
+/**
+ *The driver for the program
+ */
 public class Driver {
-	private CountryModel countryModel;
-	//TODO figure out what views need to go here
+	/**
+	 * The country from which the rest of the classes will derive their information from
+	 */
+	private CountryModel countryModel=new CountryModel();
+	
+	/**
+	 * The first view to show up.
+	 */
+	private SelectionView selectionView=new SelectionView();
+	
+	
+	/**
+	 * Constructor method for driver. Views should be added to models here, and models should be set for views here.
+	 */
+	public Driver()
+	{
+		selectionView.setModel(countryModel);
+		countryModel.addActionListener(selectionView);
+	}
+	
+	/**
+	 * The main method
+	 * @param args The program arguments
+	 */
 	public static void main (String[] args)
 	{
-		CountryModel countryModel=new CountryModel();
 	}
 	
 
---
+++ project2Folder/src/MVCStuff/SelectionView.java
@@ -0,0 +1,16 @@
+package MVCStuff;
+
+import java.awt.event.ActionEvent;
+
+public class SelectionView extends View {
+	
+	public SelectionView()
+	{
+		//TODO
+	}
+	
+	public void actionPerformed(ActionEvent e)
+	{
+		//TODO
+	}
+}
