1fb61ca5e5f476772e8fe09ceb451370861e51d0
dem1995 <belmont95@live.com>
2015-04-19 17:11:55 -0500

    Updating AddPersonView

--- project2Folder/src/MVCStuff/AddPersonView.java
+++ project2Folder/src/MVCStuff/AddPersonView.java
@@ -1,16 +1,36 @@
 package MVCStuff;
 
+import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
+
+import javax.swing.JButton;
+import javax.swing.JTextField;
+
+import countryComponents.City;
 
 public class AddPersonView extends View {
 	
 	/**
-	 * Constructor for PieChartView
+	 * The button for entering a created person into the database.
 	 */
-	public AddPersonView()
+	private JButton enterButton= new JButton();
+	
+	JTextField personName= new JTextField();
+	
+	JTextField birthDate = new JTextField();
+	
+	JTextField birthPlace= new JTextField();
+	
+	JTextField deathDate = new JTextField();
+	
+	/**
+	 * Constructor for PieChartView. Takes in a city to add the person to.
+	 */
+	public AddPersonView(City city)
 	{
 		//TODO
 		super();
+		this.setLayout(new GridLayout(2,4));
 	}
 	
 	@Override
