5907536c116ad52c5c7ff176d3b5d3437bc6f33d
dem1995 <belmont95@live.com>
2015-04-20 00:29:12 -0500

    CitySelectionView finished

---
+++ project2Folder/src/MVCStuff/CitySelectionView.java
@@ -0,0 +1,41 @@
+package MVCStuff;
+
+import java.awt.GridLayout;
+
+import javax.swing.JButton;
+import javax.swing.JList;
+import javax.swing.JScrollPane;
+
+import countryComponents.City;
+import countryComponents.State;
+
+public class CitySelectionView extends View {
+	
+	private JList<City> cityList;
+	
+	private JScrollPane cityScrollPane;
+	
+	private JButton enterButton= new JButton("Enter");
+	
+	public CitySelectionView(State state)
+	{
+		super();
+		this.setLayout(new GridLayout(2, 1));
+		cityList= new JList<City>(state.getCities().toArray(new City[0]));
+		cityScrollPane= new JScrollPane(cityList);
+		this.add(cityScrollPane);
+		cityList.setSelectedIndex(0);
+		this.add(enterButton);
+	}
+	
+	public City getSelectedCity()
+	{
+		return cityList.getSelectedValue();
+	}
+	
+	public JButton getEnterButton()
+	{
+		return enterButton;
+	}
+
+}
