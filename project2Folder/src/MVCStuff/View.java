a25cabc4544d418830df7f157f01f4bccef6c072
dem1995 <belmont95@live.com>
2015-04-20 19:40:26 -0500

    Resetting view

--- project2Folder/src/MVCStuff/View.java
+++ project2Folder/src/MVCStuff/View.java
@@ -18,6 +18,11 @@ public abstract class View extends JFrame implements ActionListener
 {
 	
 	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	/**
 	 * The TeamMateModel used by this view
 	 */
 	private CountryModel countryModel;
